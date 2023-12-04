9d1b208c1cbd561e2c58021b8c24b05adc69873b
audgns722 <audgns722@gmail.com>
2023-12-05 00:47:15 +0900

    231205집업데이트

--- src/assets/js/section2.js
+++ src/assets/js/section2.js
@@ -4,25 +4,26 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 export function section2() {
     gsap.registerPlugin(ScrollTrigger);
 
-    // 각 .horizon 요소에 대해 반복
-    document.querySelectorAll('.horizon').forEach((horizon, index) => {
+    gsap.utils.toArray('.horizon').forEach((horizon, index) => {
         const image = horizon.querySelector('.right img');
-
-        gsap.from(image, {
-            opacity: 0,
-            y: 50,
-            scale: 0.8, // 시작 시 작게 시작
-            rotate: 45, // 시작 시 45도 회전
-            ease: "power4.out", // 사용할 ease 함수
-            duration: 1,
-            scrollTrigger: {
-                trigger: horizon,
-                start: "top 20%",
-                toggleActions: "play none none none",
-            }
+    
+        gsap.from([horizon, image], {
+          opacity: 0,
+          x: 200 * (index + 1),
+          scale: 0.8,
+          rotate: 45,
+          ease: "power4.out",
+          duration: 1,
+          stagger: 0.5,
+          scrollTrigger: {
+            trigger: '.horizon',
+            start: "top center",
+            scrub: true,
+          },
         });
-    });
-
+      });
+    
+    
     const mouseCursor = document.querySelector(".mouse__cursor");
 
     gsap.to(mouseCursor, {
@@ -65,4 +66,46 @@ export function section2() {
             },
         });
     });
-}
+
+    gsap.to(".slide__text", {
+        scrollTrigger: {
+            trigger: "#section3",
+            start: "top center",
+            end: "bottom center",
+            scrub: 1
+        },
+        duration: 1,
+        x: "300px",
+        ease: "power1.out",
+        delay: 0.5,
+    });
+    
+    gsap.to(".slide__text2", {
+        scrollTrigger: {
+            trigger: "#section3",
+            start: "top 60%",    // 또는 "+=1"과 같이 이전 애니메이션이 시작된 후 1초 뒤에 시작하도록 설정할 수 있습니다.
+            end: "bottom center",   // 이 애니메이션은 .slide__text2가 화면 중앙에 도달할 때 시작됩니다.
+            scrub: 1
+        },
+        duration: 1,
+        opacity: 1,
+        delay: 2  // 이전 애니메이션이 완료된 후 2초 뒤에 시작
+    });
+
+    // section4 
+    gsap.utils.toArray('#section4 .text').forEach((text, index) => {
+        gsap.from(text, {
+          y: 100,
+          opacity: 0,
+          duration: 1,
+          scrollTrigger: {
+            trigger: '#section4',
+            start: 'top center+=100',
+            end: 'bottom center-=100',
+            toggleActions: 'play none none none',
+          },
+          delay: index * 0.5, // 순차적으로 보여주기 위한 딜레이
+        });
+      });
+    }
+
