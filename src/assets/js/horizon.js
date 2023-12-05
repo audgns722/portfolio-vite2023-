ffd7d89fd780ae5b8c2c51e4345f40de95721cff
audgns722 <144664895+audgns722@users.noreply.github.com>
2023-12-05 18:31:00 +0900

    231205

--- src/assets/js/horizon.js
+++ src/assets/js/horizon.js
@@ -3,8 +3,10 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 
 export function horizon() {
     gsap.registerPlugin(ScrollTrigger);
-    const horSection = gsap.utils.toArray(".horizon");
+    const horSection = gsap.utils.toArray(".horizon__wrap > .horizon");
     const horSection2 = gsap.utils.toArray(".sections");
+    const horizontal = document.querySelector(".horizon__wrap");
+
 
     gsap.to(horSection, {
         xPercent: -100 * (horSection.length - 1),
@@ -12,13 +14,15 @@ export function horizon() {
         scrollTrigger: {
             trigger: "#section2",
             start: "top top",
-            end: "+=9600",
+            end: () => "+=" + (horizontal.offsetWidth - innerWidth),
             pin: true,
-            scrub: 1,
+            scrub: 0.5,
+            snap: 1 / (horSection.length - 1),
             invalidateOnRefresh: true,
             anticipatePin: 1,
         }
     });
+
 
     gsap.to(horSection2, {
         xPercent: -100,
@@ -26,9 +30,10 @@ export function horizon() {
         scrollTrigger: {
             trigger: "#section5",
             start: "top top",
-            end: "+=3840",
+            end: "200%",
             pin: true,
             scrub: 1,
+            snap: 1,
             invalidateOnRefresh: true,
             anticipatePin: 1,
         }
--- src/assets/js/section2.js
+++ src/assets/js/section2.js
@@ -4,50 +4,6 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 export function section2() {
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.utils.toArray('.horizon').forEach((horizon, index) => {
-        const image = horizon.querySelector('.right img');
-    
-        gsap.from([horizon, image], {
-          opacity: 0,
-          x: 200 * (index + 1),
-          scale: 0.8,
-          rotate: 45,
-          ease: "power4.out",
-          duration: 1,
-          stagger: 0.5,
-          scrollTrigger: {
-            trigger: '.horizon',
-            start: "top center",
-            scrub: true,
-          },
-        });
-      });
-    
-    
-    const mouseCursor = document.querySelector(".mouse__cursor");
-
-    gsap.to(mouseCursor, {
-        scrollTrigger: {
-            trigger: "#section2",
-            start: "top center",
-            end: "bottom center",
-            toggleActions: "play none none none",
-        },
-        xPercent: -50, // Center the cursor horizontally
-        yPercent: -50, // Center the cursor vertically
-        ease: "power2.easeInOut",
-    });
-
-    window.addEventListener("mousemove", (e) => {
-        gsap.to(".mouse__cursor", {
-            x: e.clientX,
-            y: e.clientY,
-            duration: 0.3,
-            ease: "power2.easeInOut",
-        });
-    });
-
-
     // section1
     const elements = document.querySelectorAll("#section1 > .intro__text > *");
 
@@ -79,7 +35,7 @@ export function section2() {
         ease: "power1.out",
         delay: 0.5,
     });
-    
+
     gsap.to(".slide__text2", {
         scrollTrigger: {
             trigger: "#section3",
@@ -92,20 +48,170 @@ export function section2() {
         delay: 2  // 이전 애니메이션이 완료된 후 2초 뒤에 시작
     });
 
+    // section2
+    // progress
+    const horizontal = document.querySelector(".horizon__wrap");
+    gsap.to("progress", {
+        value: 100,
+        ease: "none",
+        scrollTrigger: {
+            scrub: 0.3,
+            trigger: "#section2",
+            start: "top bottom",
+            end: () => "+=" + (horizontal.offsetWidth),
+        }
+    })
+    // 이미지 효과 section2
+
+    const sections = gsap.utils.toArray(".horizon__wrap > .horizon");
+
+    let scrollTween = gsap.to(sections, {
+        xPercent: -100 * (sections.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: horizontal,
+            start: "top top",
+            end: () => "+=" + (horizontal.offsetWidth - innerWidth),
+            pin: true,
+            scrub: 1,
+            invalidateOnRefresh: true,
+            anticipatePin: 1
+        }
+    })
+
+    gsap.to(".horizon.s1 > .container > .right img", {
+        y: 200,
+        duration: 2,
+        ease: "elastic",
+        scrollTrigger: {
+            trigger: ".horizon.s1 > .container > .right img",
+            containerAnimation: scrollTween,
+            start: "left center",
+            toggleActions: "play none reverse none",
+            markers: true,
+            id: "img1"
+        }
+    })
+    gsap.to(".horizon.s2 > .container > .right img", {
+        y: 200,
+        duration: 2,
+        ease: "elastic",
+        scrollTrigger: {
+            trigger: ".horizon.s2 > .container > .right img",
+            containerAnimation: scrollTween,
+            start: "left center",
+            toggleActions: "play none reverse none",
+            markers: true,
+            id: "img2"
+        }
+    })
+
+    gsap.to(".horizon.s3 > .container > .right img", {
+        y: 200,
+        duration: 2,
+        ease: "elastic",
+        scrollTrigger: {
+            trigger: ".horizon.s3 > .container > .right img",
+            containerAnimation: scrollTween,
+            start: "left center",
+            toggleActions: "play none reverse none",
+            markers: true,
+            id: "img3"
+        }
+    })
+
+    gsap.to(".horizon.s4 > .container > .right img", {
+        y: 200,
+        duration: 2,
+        ease: "elastic",
+        scrollTrigger: {
+            trigger: ".horizon.s4 > .container > .right img",
+            containerAnimation: scrollTween,
+            start: "left center",
+            toggleActions: "play none reverse none",
+            markers: true,
+            id: "img4"
+        }
+    })
+
+    gsap.to(".horizon.s5 > .container > .right img", {
+        y: 200,
+        duration: 2,
+        ease: "elastic",
+        scrollTrigger: {
+            trigger: ".horizon.s5 > .container > .right img",
+            containerAnimation: scrollTween,
+            start: "left center",
+            toggleActions: "play none reverse none",
+            markers: true,
+            id: "img5"
+        }
+    })
+
+
+    // 마우스효과
+    const mouseCursor = document.querySelector(".mouse__cursor");
+    const popup = document.getElementById('nav__popup');
+
+    gsap.to(mouseCursor, {
+        scrollTrigger: {
+            trigger: "#section2",
+            start: "top center",
+            end: "bottom center",
+            toggleActions: "play none none none",
+        },
+        xPercent: -50, // Center the cursor horizontally
+        yPercent: -50, // Center the cursor vertically
+        ease: "power2.easeInOut",
+    });
+
+    window.addEventListener("mousemove", (e) => {
+        if (!popup.style.display || popup.style.display === 'none') {
+            gsap.to(".mouse__cursor", {
+                x: e.clientX,
+                y: e.clientY,
+                duration: 0.3,
+                ease: "power2.easeInOut",
+            });
+        }
+    });
+
+
     // section4 
     gsap.utils.toArray('#section4 .text').forEach((text, index) => {
         gsap.from(text, {
-          y: 100,
-          opacity: 0,
-          duration: 1,
-          scrollTrigger: {
-            trigger: '#section4',
-            start: 'top center+=100',
-            end: 'bottom center-=100',
-            toggleActions: 'play none none none',
-          },
-          delay: index * 0.5, // 순차적으로 보여주기 위한 딜레이
+            y: 100,
+            opacity: 0,
+            duration: 1,
+            scrollTrigger: {
+                trigger: '#section4',
+                start: 'top center+=100',
+                end: 'bottom center-=100',
+                toggleActions: 'play none none none',
+            },
+            delay: index * 0.5, // 순차적으로 보여주기 위한 딜레이
         });
-      });
-    }
+    });
+
+
+
+    // nav show&hide
+    // GSAP 애니메이션 코드
+    const btn = document.querySelector('.header__navmob .btn');
+    const text = document.querySelector('.header__navmob .text');
+
+    // 초기 상태 설정
+    gsap.set(popup, { autoAlpha: 0, height: 0, display: 'none' });
+
+    // 버튼 클릭 시 애니메이션
+    btn.addEventListener('click', () => {
+        if (popup.style.display === 'none') {
+            gsap.to(popup, { duration: 0.5, autoAlpha: 1, height: '100%', display: 'block', ease: 'power4.easeInOut' });
+            text.textContent = 'Close';
+        } else {
+            gsap.to(popup, { duration: 0.5, autoAlpha: 0, height: 0, display: 'none', ease: 'power4.easeInOut' });
+            text.textContent = 'Menu';
+        }
+    });
+}
 
