b0ea66570c640b863c860273ca44389710463c46
audgns722 <144664895+audgns722@users.noreply.github.com>
2023-12-15 18:09:22 +0900

    231215

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,13 +1,13 @@
-import { project } from "./section3.js";
 import { smooth } from "./smooth.js";
-import { section4 } from './section4.js';
-import { section1 } from './section1.js';
-import { section2 } from './section2.js';
+// import { project } from "./section3.js";
+// import { section4 } from './section4.js';
+// import { section1 } from './section1.js';
+// import { section2 } from './section2.js';
 
 window.addEventListener("load", function () {
-    project();
     smooth();
-    section1();
-    section4();
-    section2();
+    // project();
+    // section1();
+    // section4();
+    // section2();
 });
--- src/assets/js/scrollingMarquee.js
+++ src/assets/js/scrollingMarquee.js
@@ -1,30 +1,37 @@
-// Marquee 커스텀 함수 : 파라미터는 (선택자, 속도, 방향전환 여부)
 function Marquee(selector, speed, reverse) {
-    const parentSelector = document.querySelector(selector); // 클래스명을 인자로 받은 매개변수
-    const clone = parentSelector.innerHTML; // 해당 클래스 엘리먼트의 html 값을 clone으로 선언
-    const firstElement = parentSelector.firstElementChild;
-    let i = 0;
-    // console.log(firstElement);
-    parentSelector.insertAdjacentHTML('beforeend', clone);
-    parentSelector.insertAdjacentHTML('beforeend', clone);
+    const parentSelector = document.querySelector(selector);
+    const clone = parentSelector.innerHTML;
+    parentSelector.innerHTML += clone;
 
     if (reverse) parentSelector.classList.add('reverse');
 
-    setInterval(function () {
+    let i = 0;
+
+    function animate() {
+        const firstElement = parentSelector.firstElementChild;
+
         if (reverse) {
             firstElement.style.marginRight = `-${i}px`;
         } else {
             firstElement.style.marginLeft = `-${i}px`;
         }
+
         if (i > firstElement.clientWidth) {
             i = 0;
         }
         i += speed;
-    }, 0);
+
+        requestAnimationFrame(animate);
+    }
+
+    animate();
 }
 
-// 윈도우 로드되면 해당 함수 실행
 window.addEventListener('load', function () {
-    Marquee('.marquee2', 0.4, false);
-    Marquee('.marquee1', 0.4, true);
+    for (let i = 0; i < 2; i++) {
+        Marquee('.marquee2', 1, false);
+        Marquee('.marquee1', 1, true);
+        Marquee('.marquee4', 1, false);
+        Marquee('.marquee3', 1, true);
+    }
 });
